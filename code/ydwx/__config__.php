f71153aa13293380a1193b9fa8a43ac83933c285
leeboo <leeboo@yidianhulian.com>
2016-03-19 14:37:49 +0800

    account

--- code/ydwx/__config__.php
+++ code/ydwx/__config__.php
@@ -14,11 +14,11 @@ YDWXHook::include_files($cwd."/libs");
 define("YDWX_HOOK_DIR",             $cwd."/../ydwxhooks");
 
 //你网站的地址,以/结尾，通过YDWX_SITE_URL."ydwx/index.php"；需要能正确访问
-define("YDWX_SITE_URL",             "http://oa.yidianhulian.com/");
+define("YDWX_SITE_URL",             "");
 
 //微信网站定义，用于微信登录网站
-define("YDWX_WEIXIN_WEB_APP_ID",         "wxfe1869874a0e3ceb");
-define("YDWX_WEIXIN_WEB_APP_SECRET",     "105984d53a37811f85971689036bfbc8");
+define("YDWX_WEIXIN_WEB_APP_ID",         "");
+define("YDWX_WEIXIN_WEB_APP_SECRET",     "");
 
 //如果你想作为微信第三方托管平台
 define("YDWX_WEIXIN_COMPONENT_APP_ID",         "");
@@ -27,10 +27,10 @@ define("YDWX_WEIXIN_COMPONENT_ENCODING_AES_KEY","");
 define("YDWX_WEIXIN_COMPONENT_TOKEN",          "");
 
 //微信公众号定义
-define("YDWX_WEIXIN_APP_ID",             "wx2090e7643156cbfc");//
-define("YDWX_WEIXIN_APP_SECRET",         "3497906b89c6d409439cdef0fd0a6772 ");//
-define("YDWX_WEIXIN_ENCODING_AES_KEY",   "5dnWsxwgMU8tyA5qtMrLde2WWx9gKknLVUbeUy337yW");//
-define("YDWX_WEIXIN_TOKEN",              "FyigMYfJqSkt7Kn360B9");//
+define("YDWX_WEIXIN_APP_ID",             "");//
+define("YDWX_WEIXIN_APP_SECRET",         " ");//
+define("YDWX_WEIXIN_ENCODING_AES_KEY",   "");//
+define("YDWX_WEIXIN_TOKEN",              "");//
 
 //微信支付商户定义
 define("YDWX_WEIXIN_MCH_ID",             "");
@@ -50,8 +50,8 @@ define("YDWX_WEIXIN_APICLIENT_KEY",      "");
 define("YDWX_WEIXIN_ROOTCA",      "");
 
 //企业号的cropid
-define("YDWX_WEIXIN_CROP_ID",     "wxb4319156f6c9eccf");
-define("YDWX_WEIXIN_CROP_SECRET", "KIO6DreAw0iP0I-Q4QljpFscmQz-2u7uwhZ8BerzJGGVeUCP8HdUsqaLVHSI7z4c");
+define("YDWX_WEIXIN_CROP_ID",     "");
+define("YDWX_WEIXIN_CROP_SECRET", "");
 
 //微信支付商户定义, 默认跟上面配置的不同公众号一样
 define("YDWX_WEIXIN_QY_MCH_ID",             YDWX_WEIXIN_MCH_ID);
